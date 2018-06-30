903afa8785f5644879fc1cae5cabd9932498a25f
Ali-HP\Ali <alidal20@gmail.com>
2018-06-30 12:04:49 +0500

    Breaking a string code

---
+++ DAA_Project_Afraaz/DAA_Project_Afraaz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace breaKString
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "we are here to break a string";
+            Console.WriteLine("we are here to break a string");
+            char[] array = value.ToCharArray();
+            Console.WriteLine("Enter starting index of substring: ");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Ending index of substring: ");
+            int e = Convert.ToInt32(Console.ReadLine());
+            for (int i = s; i <= e; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
